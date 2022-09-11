b298acbc762a1b8e9c02c16e0c41570adc7d2ac8
John Terve <89539449797@mail.ru>
2022-09-11 20:48:11 +0300

    Second Task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Main {
     public static void main(String[] args) throws Exception{
         ThreadGroup group = new ThreadGroup("Group");
-        MyThread thread1 = new MyThread(group, "First");
-        MyThread thread2 = new MyThread(group, "Second");
-        MyThread thread3 = new MyThread(group, "Third");
-        MyThread thread4 = new MyThread(group, "Fourth");
+        Callable<Integer> thread1 = new MyThread(group, "First");
+        Callable<Integer> thread2 = new MyThread(group, "Second");
+        Callable<Integer> thread3 = new MyThread(group, "Third");
+        Callable<Integer> thread4 = new MyThread(group, "Fourth");
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
 
+        ExecutorService threadPull = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        Future<Integer> future = threadPull.submit(thread1);
+
+        threadPull.invokeAll(Arrays.asList(thread1, thread2, thread3, thread4));
         try {
             Thread.sleep(15_000);
         } catch (InterruptedException ex) {
             System.out.println(ex.getMessage());
         }
+        threadPull.shutdown();
         group.interrupt();
+
     }
 }
--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -1,10 +1,16 @@
-public class MyThread extends Thread{
+import java.util.concurrent.Callable;
+
+public class MyThread extends Thread implements Callable<Integer>{
+    static int messageCount = 0;
+    String name;
     public MyThread (String name){
         Thread.currentThread().setName(name);
+        this.name = name;
     }
 
     public MyThread(ThreadGroup group, String name) {
         super(group, name);
+        this.name = name;
     }
 
     @Override
@@ -18,4 +24,15 @@ public class MyThread extends Thread{
 
         }
     }
+
+    @Override
+    public Integer call() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + " 2 + " + messageCount + " = " + (2 + messageCount));
+            messageCount++;
+            Thread.sleep(500);
+        }
+        return messageCount;
+    }
+
 }
